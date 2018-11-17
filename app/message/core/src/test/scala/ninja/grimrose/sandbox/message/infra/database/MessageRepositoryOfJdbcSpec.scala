3535e6dbe13540c960aebd1e32e44c999171d68e
grimrose <grimrose@users.noreply.github.com>
2018-11-18 01:49:34 +0900

    Modify message repository
    fix order by id

--- app/message/core/src/test/scala/ninja/grimrose/sandbox/message/infra/database/MessageRepositoryOfJdbcSpec.scala
+++ app/message/core/src/test/scala/ninja/grimrose/sandbox/message/infra/database/MessageRepositoryOfJdbcSpec.scala
@@ -3,7 +3,7 @@ package ninja.grimrose.sandbox.message.infra.database
 import java.time.ZonedDateTime
 
 import cats.instances.future._
-import ninja.grimrose.sandbox.message.{ Contents, Message, MessageId, MessageRepository }
+import ninja.grimrose.sandbox.message._
 import org.scalatest.{ fixture, DiagrammedAssertions, OptionValues }
 import scalikejdbc.{ DB, NamedDB }
 import scalikejdbc.scalatest.AsyncAutoRollback
@@ -39,8 +39,44 @@ class MessageRepositoryOfJdbcSpec
     }
   }
 
-  // TODO delete
+  it should "be deleted" in { implicit session =>
+    val context = JdbcContext(session, executionContext)
 
-  // TODO findAll
+    val id      = MessageId(1)
+    val message = Message(id, Contents("sample"), ZonedDateTime.now())
+
+    val future = (for {
+      _      <- repository.store(message)
+      _      <- repository.find(id)
+      _      <- repository.remove(id)
+      entity <- repository.find(id)
+    } yield entity).run(context)
+
+    future.map { actual =>
+      assert(actual.isEmpty)
+    }
+  }
+
+  it should "be find all" in { implicit session =>
+    val context = JdbcContext(session, executionContext)
+
+    val message1 = Message(MessageId(1), Contents("sample1"), ZonedDateTime.now())
+    val message2 = Message(MessageId(2), Contents("sample2"), ZonedDateTime.now())
+
+    val future = (for {
+      _        <- repository.store(message1)
+      _        <- repository.store(message2)
+      entities <- repository.findAll()
+    } yield entities).run(context)
+
+    future.map { actual =>
+      val messages = actual.messages
+
+      assert(messages.size == 2)
+
+      assert(messages.map(_.id.value) == Seq(1, 2))
+
+    }
+  }
 
 }
